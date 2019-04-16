c980603e56f963ab18eb19b81496621515f6425e
Javier Gutiérrez Navío <gnjavie@3p.mapfre.com>
2019-04-16 16:04:44 +0200

    feat: Hechos tests para las funcionalidades de GET y LIST de SITES

--- tests/test_burp_sites.py
+++ tests/test_burp_sites.py
@@ -1,20 +1,67 @@
 import pytest 
 
 class TestSites():
-    @pytest.mark.skip
-    def test_get_sites(self):
-        pass
+    def test_list_sites(self, burp_api):
+        sites = burp_api.sites.list()
+        assert len(sites) > 0
 
-    @pytest.mark.skip
-    def test_get_folder_childrens(self):
-        pass
+    def test_list_plain_sites(self, burp_api):
+        sites = burp_api.sites.list_plain()
+        sites_normal = burp_api.sites.list()
+        assert len(sites) > len(sites_normal)
 
-    @pytest.mark.skip
-    def test_get_site_info(self):
-        pass
+    @pytest.mark.parametrize('id, exists', (
+        [1, True],
+        [2, True],
+        [7, True],
+        [123, False],
+        ["ASDFG", False],
+        [9, True]
+    ))
+    def test_get_site_info(self, burp_api, id, exists):
+        site = burp_api.sites.get(id)
+        if exists:
+            assert type(site) == dict
+            assert site['id'] == str(id)
+        else:
+            assert site == {}
+
+    @pytest.mark.parametrize('id, has_childrens', (
+        [1, True],
+        [123, False],
+        ["ASDFG", False],
+        [9, False]
+    ))
+    def test_get_folder_childrens(self, burp_api, id, has_childrens):
+        sites = burp_api.sites.get_childrens(id)
+        if has_childrens:
+            assert type(sites) == list
+            assert len(sites) > 0
+        else:
+            assert sites == []
+    
+
+    @pytest.mark.parametrize('search, expected', (
+        ["XX", True],
+        ["XAY", True],
+        ["ASDF", True],
+        ["ASDFGG", True],
+        ["gato.com", True],
+        [".com", True],
+        ["POIUUYRDSADU SAHNDSAD", False],
+        [9, False]
+    ))
+    def test_search_site(self, burp_api, search, expected):
+        sites = burp_api.sites.search(search)
+        have_sites = len(sites) > 0
+        assert have_sites == expected
 
     @pytest.mark.skip
     def test_create_folder(self):
+        pass
+
+    @pytest.mark.skip
+    def test_create_subfolder(self):
         pass
 
     @pytest.mark.skip
@@ -23,4 +70,8 @@ class TestSites():
     
     @pytest.mark.skip
     def test_fail_create_site(self):
+        pass
+
+    @pytest.mark.skip
+    def test_delete_site(self):
         pass
