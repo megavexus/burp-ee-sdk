b021e039addb87d704c359d48a3d53462e49608f
Javier Gutiérrez Navío <gnjavie@3p.mapfre.com>
2019-04-30 12:32:13 +0200

    feat: Implementado PUT y modificado push para que puedas meter URI custom

--- src/burp_enterprise_sdk/abstract.py
+++ src/burp_enterprise_sdk/abstract.py
@@ -9,14 +9,14 @@ class AbstractEndpointApi(object):
     def __init__(self, connection:Connection):
         self.connection = connection
 
-    def list(self):
+    def list(self, *args, **kwargs):
         return self.get()
 
     def get(self, id=None, uri=None, params=None):
-        if not self.ENDPOINT_GET:
-            raise NotImplementedError()
-
         if not uri:
+            if not self.ENDPOINT_GET:
+                raise NotImplementedError()
+
             uri = self.ENDPOINT_GET
             if id != None:
                 uri += str(id)
@@ -33,6 +33,18 @@ class AbstractEndpointApi(object):
             headers = {"Content-Type": "application/merge-patch+json"}
         res = self.connection.post_request(uri, data=data, headers=headers)
         return res.get("data")
+
+
+    def put(self, id, uri=None, data=None):
+        if not uri and not self.ENDPOINT_PUT:
+            raise NotImplementedError()
+        
+        if not uri:
+            uri = self.ENDPOINT_PUT + str(id)
+
+        res = self.connection.post_request(uri, data=data, headers=headers)
+        return res.get("data")
+
 
     def delete(self, id):
         if not self.ENDPOINT_DELETE:
